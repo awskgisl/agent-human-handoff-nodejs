2ac42859ecdec4692ebbd0b81e82c23f55290eec
awskgisl <43135663+awskgisl@users.noreply.github.com>
2018-09-18 12:29:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -63,6 +63,6 @@ app.get('/operator', (req, res) => {
 
 // Begin responding to websocket and http requests
 messageRouter.handleConnections();
-http.listen(443, () => {
-  console.log('Listening on *:443');
+http.listen(3000, () => {
+  console.log('Listening on *:3000');
 });
